8dc37585386e229e833c71b7271cf18f6e429593
lovitesfay <lovitesfay@gmail.com>
2026-05-15 15:18:04 -0500

    added the getter and setters to the contract class

--- src/main/java/Contract.java
+++ src/main/java/Contract.java
@@ -14,4 +14,36 @@ protected double payment;
         this.totalPrice = totalPrice;
         this.payment = payment;
     }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getCustomerName() {
+        return CustomerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        CustomerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return CustomerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        CustomerEmail = customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(String vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
 }
